5f8462c0e7fce0f7b1ee09623141314ca3a08f96
Glauber Ramos <glauberamos@gmail.com>
2024-02-19 12:27:15 -0300

    Add words count

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+function countWords(str) {
+  const words = str.trim().split(/\s+/).length;
+  document.getElementById("words").innerText = words + " words";
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-  var textarea = document.getElementById("paste");
-  textarea.scrollTop = 0;
+  var paste = document.getElementById("paste");
+  paste.scrollTop = 0;
+  paste.innerHTML = localStorage.getItem("paste");
+  countWords(paste.innerText);
+
+  if (paste.addEventListener) {
+    paste.addEventListener(
+      "input",
+      function () {
+        localStorage.setItem("paste", paste.innerHTML);
+        countWords(paste.innerText);
+      },
+      false
+    );
+  } else if (paste.attachEvent) {
+    paste.attachEvent("onpropertychange", function () {
+      localStorage.setItem("paste", paste.innerHTML);
+      countWords(paste.innerText);
+    });
+  }
 });
